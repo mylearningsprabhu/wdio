446ffc7db9ba9c2880d3d7025883cca2a6e2eaf8
Prbahu <mylearningsprabhu@gmail.com>
2023-08-30 15:06:15 +0530

    dc

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -37,7 +37,7 @@ export const config: Options.Testrunner = {
     // will be called from there.
     //
     specs: [
-        './test/specs/**/*.js'
+        './test/specs/**/*.ts'
     ],
     // Patterns to exclude.
     exclude: [
